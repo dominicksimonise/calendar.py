de1f94bb51cabd3effa006ea27abe71b72dcf604
Dominick Simonise <150468410+dominicksimonise@users.noreply.github.com>
2024-02-25 21:51:22 -0600

    Add files via upload

---
+++ mycalendar.py
@@ -0,0 +1,11 @@
+# Import the calendar module
+import calendar 
+
+# user input for month and year 
+year = int(input("Enter the Year: "))
+month= int(inout("Enter the Month : "))
+
+# Display the calendar 
+
+#print(calendar.month(year,month))
+print(calendar.calendar(year))
